b7e441a752e3939391c0ca06208b4c99a6f3d983
kchondr <khondralis@gmail.com>
2025-07-20 00:31:03 +0300

    clean project

--- Inc/systick.h
+++ Inc/systick.h
@@ -2,17 +2,6 @@
 #define SYSTICK_H
 
 
-/*
- * We can calculate the delay that N cycles can achieve with the following formula:
- *   --> Delay (sec) = N_cycles * (1 / clock speed)
- *
- * So, for 1ms = 0.001 sec delay we have,
- *   => N = clock_speed * delay
- *   => N = 16000000 * 0.001
- *   => N = 16000
- * */
-#define LOAD_VALUE		16000
-
 void InitSysTick();
 void SysTickDelay(int delay_ms);
 void DeInitSysTick();
